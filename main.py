c0e78988146edcd9bbe03028247b455b7921f2e1
Brady Richmond <richmond.brady@gmail.com>
2024-06-15 16:38:19 -0700

    posting google dns ping and gateway ping now

--- main.py
+++ main.py
@@ -5,35 +5,59 @@ from uuid import uuid4
 import sys
 import json
 
+google_dns = "8.8.8.8"
 
 def main():
     print("Starting application")
     global endpoint
     endpoint = sys.argv[1]
-    ping_google_dns()
+    global gateway
+    gateway = sys.argv[2]
+    start_pinging()
 
-def ping_google_dns():
-    ping_success = subprocess.run(["ping", "-c", "1", "8.8.8.8"], capture_output=True)
+def ping(ip):
+    ping_success = subprocess.run(["ping", "-c", "1", ip], capture_output=True)
         
     if (ping_success.returncode == 0):
         ping_success_string = str(ping_success.stdout)
         time_value_index = ping_success_string.index('time') + 5
         latency = ping_success_string[time_value_index:].split(' ')[0]
-        send_success(latency)
+        return latency
     else:
         print('failure')
+        return float('inf')
 
+def start_pinging():
+    gateway = ping_gateway()
+    google = ping_google_dns()
+    send_values([gateway, google])
     sleep(10)
-    ping_google_dns()
+    start_pinging()
 
-def send_success(latency):
+def ping_google_dns():
+    return ping(google_dns)
+
+def ping_gateway():
+    return ping(gateway)
+
+def send_values(values):
     id = uuid4()
     ping_time = round(time() * 1000)
+
     data = json.dumps({
         "id": str(id),
         "ping_time": str(ping_time),
-        "latency": str(latency)
+        "gateway": {
+            "latency": values[0],
+            "ip": "Gateway"
+        },
+        "google": {
+            "latency": values[1],
+            "ip": google_dns
+        }
     })
+
+    # print(data)
 
     try:
         print('sending db update')
